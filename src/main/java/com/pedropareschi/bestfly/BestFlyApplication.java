6d72f108d7c07025504063f73f286c4388fc3fa3
pedropareschi <pedropareschi.dev@gmail.com>
2026-02-04 14:39:01 -0300

    Initial commit

---
+++ src/main/java/com/pedropareschi/bestfly/BestFlyApplication.java
@@ -0,0 +1,13 @@
+package com.pedropareschi.bestfly;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BestFlyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BestFlyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pedropareschi/bestfly/BestFlyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pedropareschi.bestfly;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BestFlyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
